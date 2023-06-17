667f544c08136a4fbfd77452c2689e27ab56b755
jarrydharris <mail@jarrydharris.org>
2023-06-17 14:33:34 +1000

    feat: api_process_text returns list of word properties

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 test:
 	@cd backend/src/tests && pytest
+
+run-debug:
+	@flask --app backend/src/api run --debug --reload
--- backend/src/text_to_token.py
+++ backend/src/text_to_token.py
@@ -1,5 +1,19 @@
+import spacy
+from spacy.tokens import Token
+
 from backend.src.models import Text
 
 
+def convert_to_word_dict(token: Token) -> dict[str, str]:
+    properties = {
+        "word": token.text_with_ws,
+        "class": token.pos_,
+    }
+    return properties
+
+
 def process_text(text: Text) -> list[dict]:
-    return text.json()
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text.dict()["text"])
+    tokens = [convert_to_word_dict(token) for token in doc]
+    return tokens
